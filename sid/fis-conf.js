6db7ca1b9f95c23edaa9af37dbfb1c50a1860560
SidKwok <oceankwok@hotmail.com>
2016-05-13 20:29:42 +0800

    done part 2

---
+++ sid/fis-conf.js
@@ -0,0 +1,34 @@
+// default settings. fis3 release
+
+// Global start
+fis.match('*.{js,css}', {
+  useHash: true
+});
+
+fis.match('::image', {
+  useHash: true
+});
+
+fis.match('*.js', {
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  optimizer: fis.plugin('png-compressor')
+});
+
+// Global end
+
+// default media is `dev`
+fis.media('dev')
+  .match('*', {
+    useHash: false,
+    optimizer: null
+  });
+
+// extends GLOBAL config
+fis.media('production');

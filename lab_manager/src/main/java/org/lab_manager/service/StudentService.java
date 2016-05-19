e223bdc4920c3ffe0e0659bd202f45da0ab18a28
xiaofeige <1149151981@qq.com>
2016-05-19 21:23:35 +0800

    adjust front end

---
+++ lab_manager/src/main/java/org/lab_manager/service/StudentService.java
@@ -0,0 +1,7 @@
+package org.lab_manager.service;
+
+/**
+ * Created by xiaofeige on 2016/5/19.
+ */
+public interface StudentService {
+}
---
+++ lab_manager/src/main/java/org/lab_manager/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by xiaofeige on 2016/5/19.
+ */
+package org.lab_manager.service;

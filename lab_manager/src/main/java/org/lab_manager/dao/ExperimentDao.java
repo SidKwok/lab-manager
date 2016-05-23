6a00de3a06e5348f90d6a1c3038e04f2b4b612d3
SilenceMrWang <wangrupeng@live.cn>
2016-05-23 20:14:54 +0800

    ExperimentDao

---
+++ lab_manager/src/main/java/org/lab_manager/dao/ExperimentDao.java
@@ -0,0 +1,14 @@
+package org.lab_manager.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.lab_manager.entity.Experiment;
+
+/**
+ * Created by Silence on 2016/5/23.
+ */
+public interface ExperimentDao {
+    public Experiment queryById(String id);
+    public Experiment queryByIdAndName(@Param("id") String id,@Param("name") String name);
+    public Experiment addExperiment(Experiment experiment);
+
+}

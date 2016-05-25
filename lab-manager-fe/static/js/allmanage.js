c05d200fbc80cf8f0d7f4bf24ae459207ccebca1
SidKwok <oceankwok@hotmail.com>
2016-05-25 15:47:47 +0800

    finish login

--- lab-manager-fe/static/js/allmanage.js
+++ lab-manager-fe/static/js/allmanage.js
@@ -1,11 +1,19 @@
+// 全局变量
+var userInfo = window.location.href.split('?')[1];
+var role = userInfo.split('&')[1].split('=')[1]; // role
+var username = userInfo.split('&')[0].split('=')[1];; // username
+
 /**
 * 页面初始化事件
 */
-
 function init () {
-
-    var role = 'student'; // role
-    var username = 'sid'; // username
+    /**
+    * 初始化导航栏
+    */
+    $($('.nav-location')[0]).attr('href', '../info/index.html?' + userInfo + '?');
+    $($('.nav-location')[1]).attr('href', '../promanage/index.html?' + userInfo + '?');
+    $($('.nav-location')[2]).attr('href', '../dailyjob/index.html?' + userInfo + '?');
+    $($('.nav-location')[3]).attr('href', '../aboutus/index.html?' + userInfo + '?');
 
     /**
     * 初始化实验室卡片
--- lab-manager-fe/static/js/dailyjob.js
+++ lab-manager-fe/static/js/dailyjob.js
@@ -1,15 +1,25 @@
+// 全局变量
+var userInfo = window.location.href.split('?')[1];
+var role = userInfo.split('&')[1].split('=')[1]; // role
+var username = userInfo.split('&')[0].split('=')[1];; // username
+
 /**
 * 页面初始化事件
 */
-
 function init () {
+    /**
+    * 初始化导航栏
+    */
+    $($('.nav-location')[0]).attr('href', '../info/index.html?' + userInfo + '?');
+    $($('.nav-location')[1]).attr('href', '../allmanage/index.html?' + userInfo + '?');
+    $($('.nav-location')[2]).attr('href', '../promanage/index.html?' + userInfo + '?');
+    $($('.nav-location')[3]).attr('href', '../aboutus/index.html?' + userInfo + '?');
     loadNotice();
 }
 
 /**
 * 加载公告
 */
-
 function loadNotice () {
     $.ajax({
         type: 'GET',

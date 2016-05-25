022b21c85fd83053e58fd498535834c4179a861f
SilenceMrWang <wangrupeng@live.cn>
2016-05-25 10:27:20 +0800

    labInfo

--- lab_manager/src/main/java/org/lab_manager/dao/LabInfoDao.java
+++ lab_manager/src/main/java/org/lab_manager/dao/LabInfoDao.java
@@ -2,10 +2,12 @@ package org.lab_manager.dao;
 
 import org.lab_manager.entity.LabInfo;
 
+import java.util.ArrayList;
+
 /**
  * Created by Silence on 2016/5/23.
  */
 public interface LabInfoDao {
     public LabInfo queryById(Integer id);
-
+    public ArrayList<LabInfo> queryAll();
 }

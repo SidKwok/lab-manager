9936703bb18f5649bb9d108fce67d782da99dabe
SidKwok <oceankwok@hotmail.com>
2016-05-25 19:21:27 +0800

    confirm login

--- lab-manager-fe/static/js/login.js
+++ lab-manager-fe/static/js/login.js
@@ -9,24 +9,41 @@ function basicEvent () {
             username: username,
             password: $('#password').val()
         };
-        window.location.href = './info/index.html?username=' + username + '&role=teacher?';
-        // $.ajax({
-        //     type: 'POST',
-        //     url: '_login',
-        //     data: params,
-        //     dataType: 'json',
-        //     success: function(retData){
-        //         if (retData.status === 'success') {
-        //             window.location.href = '/info/index.html?username=' + username + '&role=' + retData.role;
-        //         } else {
-        //             alert('登录失败，原因：' + retData.status)
-        //         }
-        //     },
-        //     error: function() {
-        //         console.log('_login fail');
-        //         alert('后台错误');
-        //     }
-        // });
+        // console.log(params);
+        // window.location.href = './info/index.html?username=' + username + '&role=teacher?';
+        $.ajax({
+            type: 'POST',
+            url: '_login',
+            data: params,
+            dataType: 'json',
+            success: function(retData){
+                if (retData.status === 'success') {
+                    window.location.href = './info/index.html?username=' + username + '&role=' + retData.role;
+                } else {
+                    alert('登录失败，原因：' + retData.status)
+                }
+            },
+            error: function() {
+                console.log('_login fail');
+                alert('后台错误');
+
+                // /***************************************
+                // * 用于前端test 测试状态：
+                // */
+                // /*ajax返回的数据*/
+                // var retData = {
+                //   "status": "success",
+                //   "role": "student"
+                // };
+                // /**************/
+                // if (retData.status === 'success') {
+                //     window.location.href = './info/index.html?username=' + username + '&role=' + retData.role;
+                // } else {
+                //     alert('登录失败，原因：' + retData.status)
+                // }
+                // /***************************************/
+            }
+        });
     });
 }
 basicEvent();

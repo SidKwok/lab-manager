68d783f410ee4692e611c3d9777e41cdef5ba3a1
xiaofeige <1149151981@qq.com>
2016-05-27 21:09:53 +0800

    go to test

---
+++ lab_manager/src/main/java/org/lab_manager/service/ITeachService.java
@@ -0,0 +1,20 @@
+package org.lab_manager.service;
+
+/**
+ * Created by xiaofeige on 2016/5/27.
+ */
+public interface ITeachService {
+    /**
+     * 上传学生成绩
+     */
+    public boolean uploadStuGrade();
+
+    /**
+     * 更新学生考勤
+     */
+    public boolean updateStuAttendence();
+
+    /**
+     * 
+     */
+}
